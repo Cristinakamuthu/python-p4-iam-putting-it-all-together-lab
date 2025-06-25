c4d239a75b5cd96faf0e4d9ba95452852d4fc98c
Cristina <kamuthucristina@gmail.com>
2025-06-25 12:17:02 +0300

    doneeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee!

---
+++ server/migrations/versions/bd88b6356715_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: bd88b6356715
+Revises: 
+Create Date: 2025-06-25 12:09:10.354339
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'bd88b6356715'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
